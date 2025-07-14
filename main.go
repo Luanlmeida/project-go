918b8c06c6a45ce869a22c962ff7cd3ce03b6951
Luanlmeida <luanlmeidaa7@gmail.com>
2025-07-14 13:20:07 -0300

    Update main.go

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	var taskOne = "Watch Golang Full course"
+
 	fmt.Println("##### Welcome to Todolist App ##### \n")
 
 	fmt.Println("List of my Todos")
-	fmt.Println("Watch Golang Full course")
+	fmt.Println("Watch Go crash course")
+	fmt.Println(taskOne)
+	fmt.Println("Reward my self with a donut")
 
 	fmt.Println()
 	fmt.Println("Tutorials")
 	fmt.Println("Watch Go crash course")
+	fmt.Println(taskOne)
 
 	fmt.Println()
 	fmt.Println("Rewards")
