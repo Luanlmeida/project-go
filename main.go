2e820d969eed8bdd31e37af7d318be968eb58873
Luanlmeida <luanlmeidaa7@gmail.com>
2025-07-14 13:25:47 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,20 +7,10 @@ func main() {
 	var shortGolang = "Watch Go crash course"
 	var rewardDessert = "Reward my self with a donut"
 
-	fmt.Println("##### Welcome to Todolist App ##### \n")
+	fmt.Println("##### Welcome to our Todolist App! #####")
 
 	fmt.Println("List of my Todos")
 	fmt.Println(shortGolang)
 	fmt.Println(fullGolang)
 	fmt.Println(rewardDessert)
-
-	fmt.Println()
-	fmt.Println("Tutorials")
-	fmt.Println(shortGolang)
-	fmt.Println(fullGolang)
-
-	fmt.Println()
-	fmt.Println("Rewards")
-	fmt.Println(rewardDessert)
-
 }
